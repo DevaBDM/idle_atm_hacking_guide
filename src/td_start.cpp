a67b6644cd7df5811559fe9a7a9db3abb5f973b4
DevaBDM <67018901+DevaBDM@users.noreply.github.com>
2021-02-18 12:11:31 +0300

    cerr info added

--- src/td_start.cpp
+++ src/td_start.cpp
@@ -114,7 +114,9 @@ class TdExample {
                     process_response(client_manager_->receive(10));
                   } else {
                     if ( response_called == 2000000 ){
+                      std::cerr << "wait " << time_To_wait ;
                       sleep(time_To_wait);
+                      std::cerr << " -> ";
                       time_To_wait+=2;
                       response_called=0;
                     } else {
